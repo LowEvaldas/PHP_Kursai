67eda5cc904b6f78a1cccf94f6c9013433bfcd86
DESKTOP-NFPHQ3P\Vartotojas <lowevaldas@gmail.com>
2018-04-19 17:02:30 +0300

    Namų darbai su poromis ir mokiniais. 2018-04-17

---
+++ namu_darbai/2018-04-17/duomenys.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vartotojas
+ * Date: 2018.04.18
+ * Time: 15:55
+ */
+
+
+// Jonas, Ona, Petras + 0,1,2
+// Jonas Ona Maryte + 0,1 3
+// Jonas Ona Egle + 0 1 4
+// Jonas Petras maryte + 0 2 3
+// Jonas petras egle 0 2 4
+// Jonas maryte egle 0 3 4
+// Ona petras maryte 1 2 3
+// ona petras egle 1 2 4
+// petras maryte egle 2 3 4
+$zmones = [
+    ['vardas' => 'Jonas', 'lytis' => 'V'],
+    ['vardas' => 'Ona', 'lytis' => 'M'],
+    ['vardas' => 'Petras', 'lytis' => 'V'],
+    ['vardas' => 'Marytė', 'lytis'=> 'M'],
+    ['vardas' => 'Eglė', 'lytis' => 'M']
+];
+
+
+$mokiniai = [
+    ['vardas' => 'Jonas', 'pazymiai' => [
+        'lietuviu' => [4, 8, 6, 7], 'anglu' => [6, 7, 8],
+        'matematika' => [3, 5, 4]]],
+    ['vardas' => 'Ona', 'pazymiai' => [
+        'lietuviu' => [10, 9, 10], 'anglu' => [9, 8, 10],
+        'matematika' => [10, 10, 9, 9]]],
+ ];
+
+
